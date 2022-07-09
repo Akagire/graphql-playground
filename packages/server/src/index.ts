487826f703cdd031cea065361294356a8d10623c
Takuya Eguchi <seal2501@gmail.com>
2022-07-09 23:22:40 +0900

    refactor: follow graphQL way

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,29 @@
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import http from 'http';
+
+// @ts-ignore
+// typeDefs の型をどう持ってくるかぱっとわからず。チュートリアルのまま進める
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app, path: '/' });
+
+  await new Promise<void>(resolve => httpServer.listen({ port: 3001 }, resolve));
+  console.log(`Server ready at http://localhost:3001${server.graphqlPath}`);
+}
 
 const typeDefs = gql`
   type Book {
@@ -29,19 +53,4 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  csrfPrevention: true,
-  cache: 'bounded',
-});
-
-const app = express();
-
-server.start().then(() => {
-  server.applyMiddleware({ app });
-});
-
-app.listen(3001, () => {
-  console.log(`Express server ready at http://localhost:3001${server.graphqlPath}`);
-});
+startApolloServer(typeDefs, resolvers);
